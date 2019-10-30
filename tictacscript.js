e747c3d77a09009ca2272818af1c2e82fa6d607b
Nathaniel <nathanielmelman@gmail.com>
2019-10-30 19:11:42 +0000

    Very early beginnings of object

---
+++ tictacscript.js
@@ -0,0 +1,19 @@
+var gameBoard = {
+  1: false,
+  2: false,
+  3: false,
+  4: false,
+  5: false,
+  6: false,
+  7: false,
+  8: false,
+  9: false
+};
+
+const gameBoard = (() {
+    
+})()
+
+const personFactory = (name, marker) => {
+  return { name, marker };
+};
