106ac16804e457fb242de7eb34817046e52d284b
Nathaniel <nathanielmelman@gmail.com>
2019-10-31 08:16:14 +0000

    Starting to build out the game box

--- tictacscript.js
+++ tictacscript.js
@@ -1,19 +1,28 @@
 var gameBoard = {
-  1: false,
-  2: false,
-  3: false,
-  4: false,
-  5: false,
-  6: false,
-  7: false,
-  8: false,
-  9: false
+  1: "X",
+  2: "",
+  3: "",
+  4: "",
+  5: "X",
+  6: "",
+  7: "",
+  8: "O",
+  9: "",
 };
 
-const gameBoard = (() {
+// const gameBoard = (() {
     
-})()
+// })()
 
 const personFactory = (name, marker) => {
   return { name, marker };
 };
+
+container = document.getElementById("tictacbox")
+Object.values(gameBoard).forEach((value) => {
+  var box = document.createElement("DIV")
+  box.classList.add("symbol-box")
+  var symbol = document.createTextNode(value)
+  box.appendChild(symbol)
+  container.appendChild(box);
+})
