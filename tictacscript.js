2d7b3ec10be55b30ae27cae198b4d9474a859a2b
Nathaniel <nathanielmelman@gmail.com>
2019-11-01 18:12:51 +0000

    Added display box and dom to add X on click

--- tictacscript.js
+++ tictacscript.js
@@ -1,28 +1,57 @@
 var gameBoard = {
-  1: "X",
+  1: "",
   2: "",
   3: "",
   4: "",
-  5: "X",
+  5: "",
   6: "",
   7: "",
-  8: "O",
-  9: "",
+  8: "",
+  9: ""
 };
 
-// const gameBoard = (() {
-    
-// })()
+const gameFncs = (() {
+  const switchPlayer = () => {
+    if (currentPlayer === player1){
+      currentPlayer = player2
+    }
+    else {
+      currentPlayer = player1
+    }
+
+  }
+
+})()
 
 const personFactory = (name, marker) => {
   return { name, marker };
 };
 
-container = document.getElementById("tictacbox")
-Object.values(gameBoard).forEach((value) => {
-  var box = document.createElement("DIV")
-  box.classList.add("symbol-box")
-  var symbol = document.createTextNode(value)
-  box.appendChild(symbol)
+
+var player1 = personFactory("Player1", "X")
+var player2 = personFactory("Player2", "O")
+var currentPlayer = player1;
+
+
+
+container = document.getElementById("tictacbox");
+Object.entries(gameBoard).forEach(entry => {
+  var box = document.createElement("DIV");
+  box.classList.add("symbol-box");
+  box.setAttribute("id", entry[0]);
+  box.addEventListener("click", function(){
+    playerTurn("X", entry[0])
+  });
+  var symbol = document.createTextNode(entry[1]);
+  box.appendChild(symbol);
   container.appendChild(box);
-})
+});
+
+function playerTurn(marker, id) {
+  box = document.getElementById(id);
+  currentText = box.textContent;
+  if (currentText === "") {
+    symbol = document.createTextNode(marker);
+    box.appendChild(symbol);
+  }
+}
